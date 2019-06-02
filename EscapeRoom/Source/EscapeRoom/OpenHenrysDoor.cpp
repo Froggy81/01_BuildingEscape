0357d34538cfc642074505d19688b36dc8df492c
Paul Barker <paulallenbarker@gmail.com>
2019-06-02 22:51:23 +0930

    Added walls and door

--- EscapeRoom/Source/EscapeRoom/OpenHenrysDoor.cpp
+++ EscapeRoom/Source/EscapeRoom/OpenHenrysDoor.cpp
@@ -1,10 +1,10 @@
 // Copy this and I'll stab you
 
-
-#include "OpenDoor.h"
+#include "OpenHenrysDoor.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
-UOpenDoor::UOpenDoor()
+UOpenHenrysDoor::UOpenHenrysDoor()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -15,20 +15,25 @@ UOpenDoor::UOpenDoor()
 
 
 // Called when the game starts
-void UOpenDoor::BeginPlay()
+void UOpenHenrysDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// Find the owning Actor of this door
+	// Find Object
 	AActor* Owner = GetOwner();
 
+	// Create a rotator
+	FRotator NewRotation = FRotator(0.0f, -45.0f, 0.0f);
+
+	// Set the door rotatio
+	Owner->SetActorRotation(NewRotation);
 
 	
 }
 
 
 // Called every frame
-void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UOpenHenrysDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
